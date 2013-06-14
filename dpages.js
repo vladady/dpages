098028cb926519f6dd75ba716ff67535e3cd5ee6
Vlad Buda <vb@cylex.ro>
2013-06-14 15:10:57 +0300

    Modified completely

--- dpages.js
+++ dpages.js
@@ -1,15 +1,27 @@
 (function ($) {
   $(document).ready(function(){
     $('ul.blocks').find('li').draggable({
-      //connectWith: ".column"
-      revert: true,
+      //connectWith: ".column",
+      connectToSortable: ".column",
+      //revert: true,
+      helper: 'clone'
     });
+
     $('.column').droppable({
       drop: handleDropEvent,
     });
 
+    $('.column').sortable({
+
+    });
+
     function handleDropEvent(event, ui){
-      console.log(ui);
+      //console.log(ui);
+      id = $(this).attr('data-id');
+      //dragged = ($(ui.draggable));
+      $(this).children().each(function(){
+        console.log($(this));
+      });
     }
   });
 })(jQuery);
