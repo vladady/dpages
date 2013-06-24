05272c908b18048cf12480aaa43a02b85f314c88
vlad <vb@cylex.ro>
2013-06-24 23:39:35 +0300

    Dpages

--- dpages.conf.php
+++ dpages.conf.php
@@ -11,8 +11,8 @@
     'type' => 'block',
     'module' => 'system',
     'delta' => 'main-menu',
-    'parent' => '2',
-    'weight' => 3,
+    'parent' => '51c5fa533ed3e',
+    'weight' => 1,
     'width' => '12',
   ),
   2 => 
@@ -48,15 +48,15 @@
     'module' => 'node',
     'delta' => 'recent',
     'parent' => '3',
-    'weight' => 0,
+    'weight' => 2,
     'width' => '12',
   ),
   8 => 
   array (
-    'id' => 8,
+    'id' => '8',
     'type' => 'row',
-    'parent' => 2,
-    'weight' => 6,
+    'parent' => '2',
+    'weight' => 7,
   ),
   9 => 
   array (
@@ -98,7 +98,7 @@
     'type' => 'block',
     'cid' => '47',
     'parent' => '2',
-    'weight' => 5,
+    'weight' => 6,
     'width' => '12',
   ),
   '51c2db8fc58b2' => 
@@ -116,7 +116,7 @@
     'type' => 'block',
     'cid' => '48',
     'parent' => '2',
-    'weight' => 4,
+    'weight' => 5,
     'width' => '12',
   ),
   '51c3036875fb7' => 
@@ -134,7 +134,7 @@
     'type' => 'block',
     'cid' => '49',
     'parent' => '3',
-    'weight' => 1,
+    'weight' => 4,
     'width' => '12',
   ),
   '51c307a68e965' => 
@@ -161,7 +161,77 @@
     'type' => 'block',
     'cid' => '49',
     'parent' => '2',
-    'weight' => '2',
+    'weight' => 2,
     'width' => '12',
+  ),
+  '51c5fa533ed3e' => 
+  array (
+    'id' => '51c5fa533ed3e',
+    'type' => 'span',
+    'parent' => '51c5fa533ecdd',
+    'width' => 6,
+    'weight' => 0,
+  ),
+  '51c5fa533ed54' => 
+  array (
+    'id' => '51c5fa533ed54',
+    'type' => 'span',
+    'parent' => '51c5fa533ecdd',
+    'width' => 6,
+    'weight' => 1,
+  ),
+  '51c5fa533ecdd' => 
+  array (
+    'id' => '51c5fa533ecdd',
+    'type' => 'row',
+    'parent' => '10',
+    'weight' => 4,
+  ),
+  '51c5fa929ff6b' => 
+  array (
+    'id' => '51c5fa929ff6b',
+    'type' => 'block',
+    'cid' => '63',
+    'parent' => '10',
+    'weight' => '3',
+  ),
+  '51c5fa988402e' => 
+  array (
+    'id' => '51c5fa988402e',
+    'type' => 'block',
+    'cid' => '59',
+    'parent' => '51c5fa533ed3e',
+    'weight' => 0,
+  ),
+  '51c5fa9d66af0' => 
+  array (
+    'id' => '51c5fa9d66af0',
+    'type' => 'block',
+    'cid' => '66',
+    'parent' => '51c5fa533ed54',
+    'weight' => 0,
+  ),
+  '51c5fc26149f6' => 
+  array (
+    'id' => '51c5fc26149f6',
+    'type' => 'block',
+    'cid' => '65',
+    'parent' => '3',
+    'weight' => 3,
+  ),
+  '51c601f77bf02' => 
+  array (
+    'id' => '51c601f77bf02',
+    'type' => 'block',
+    'cid' => '69',
+    'parent' => '2',
+    'weight' => 3,
+  ),
+  '51c6a73fc98cf' => 
+  array (
+    'id' => '51c6a73fc98cf',
+    'type' => 'active_content',
+    'parent' => '3',
+    'weight' => 0,
   ),
 );
--- dpages.js
+++ dpages.js
@@ -1,80 +1,107 @@
 (function ($) {
-  $(document).ready(function(){
-    $('ul.blocks').find('div').draggable({
-      //connectWith: ".column",
-      //start: handleDragEvent,
-      connectToSortable: ".column",
-      revert: 'invalid',
-      helper: 'clone',
-    });
-
-    /*$('.column').droppable({
-      drop: handleDropEvent,
-    });*/
+  Drupal.behaviors.dpages = {
+     attach: function(context, settings){
+      $('ul.blocks').find('div').draggable({
+        connectToSortable: ".column",
+        revert: 'invalid',
+        helper: 'clone'
+      });
 
 
-    $('.column').sortable({
-      items: '.box',
-      connectWith: ".column",
-      helper: function(){
-        return $('<div>mini helper</div>');
-      },
-      sort: function(event, ui){
-        event.stopPropagation();
-      },
-      update: function(event, ui){
-      },
-      stop: function (event, ui){
-        parent = $(ui.item).parent().attr('id');
-        id = $(ui.item).attr('id');
-        cid = $(ui.item).attr('data-cid');
-        type = $(ui.item).attr('data-type');
-        weight = $(ui.item).parent().children().index($(ui.item));
-        width = 12;
+      $('.column').sortable({
+        items: '.box',
+        connectWith: ".column",
+        helper: function(){
+          return $('<div>mini helper</div>');
+        },
+        sort: function(event, ui){
+          event.stopPropagation();
+        },
+        update: function(event, ui){
+        },
+        stop: function (event, ui){
+          var parent = $(ui.item).parent().attr('id'),
+              id = $(ui.item).attr('id'),
+              cid = $(ui.item).attr('data-cid'),
+              type = $(ui.item).attr('data-type'),
+              weight = $(ui.item).parent().children().index($(ui.item)),
+              width = 12;
 
-        weights = [];
-        $(ui.item).parent().children().each(function(){
-          weights.push($(this).attr('id'));
-        });
+          weights = [];
+          $(ui.item).parent().children().each(function(){
+            weights.push($(this).attr('id'));
+          });
 
-
-        $.ajax({
-          type: 'POST',
-          url: Drupal.settings.basePath + 'dpages_box',
-          data: {
-            'item': {
-              'id': id,
-              'parent': parent,
-              'type': type,
-              'cid': cid,
-              'weight': weight,
-              'width': width
+          $.ajax({
+            type: 'POST',
+            url: Drupal.settings.basePath + 'dpages_box',
+            data: {
+              'item': {
+                'id': id,
+                'parent': parent,
+                'type': type,
+                'cid': cid,
+                'weight': weight
+                //'width': width
+              },
+              'weights': weights
             },
-            'weights': weights
-          },
-          success: function (msg) {
-            //console.log(msg.html)
-            $(ui.item).replaceWith(msg.html);
+            error: function(msg, status, error){
+              //console.log('error');
+            },
+            success: function (msg) {
+              //console.log(msg.html);
+              $(ui.item).replaceWith(msg.html);
+            }
+          });
+        }
+      });
+
+      $('ul, li').disableSelection();
+
+      //Limit resizing
+      $('.column').resizable({
+        grid: [20],
+        handles: 'e',
+        stop: function(event, ui){
+          var old_class,
+            new_class,
+            classes = $(this).attr('class');
+
+          cur_span = spanWidth(classes);
+          old_class = 'span' + cur_span;
+
+          right_el = $(this).siblings('.column').first();
+          right_span = spanWidth(right_el.attr('class'));
+          max_span = cur_span + right_span - 1;
+          console.log(max_span);
+
+          //Count closest widths
+          percent = 100 * ui.size.width / ui.originalSize.width;
+          cur_span = parseInt(cur_span * percent / 100, 10);
+
+          if(cur_span >= 1 && cur_span <= max_span){
+            new_class = 'span' + cur_span;
+            right_class = 'span' + (max_span - cur_span + 1);
+
+            $(this).removeClass(old_class)
+              .addClass(new_class);
+
+            $(right_el).removeClass('span' + right_span)
+              .addClass(right_class);
           }
-        });
+          $(this).removeAttr('style');
+          //Resize other columns in the row
+        }
+      });
+
+      function spanWidth(classes){
+        re = new RegExp("span([0-9]{1})"),
+        matches = re.exec(classes);
+        width = parseInt(matches[1], 10);
+
+        return width;
       }
-    });
-    $('ul, li').disableSelection();
-
-    /*
-     function handleDragEvent(event, ui) {
-      return;
-    }*/
-
-    function handleDropEvent(event, ui) {
-      return;
-      //console.log($(ui.draggable));
-      id = $(this).attr('data-id');
-
-      helper = $(ui.helper).html()
-      $(ui.draggable).replaceWith(helper);
-      box_id = $(ui.draggable).attr('data-id');
-      //console.log($(this).find('li').index($(ui.draggable)));
     }
-  });
+  };
 })(jQuery);
